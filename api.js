ecd58b360f7b03f46e17cc37206529607f0d4d22
Dynylson <dynylsonjr@gmail.com>
2022-05-01 16:22:28 -0300

    javascript finalizado

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 // Substitua o CHAVE_DA_API com a chave gerada no site.
 
-export const API_KEY =
-  "https://api.themoviedb.org/3/movie/550?api_key=ee6c522f6ee1372ba637b097a93e6d60";
+export const API_KEY = "ee6c522f6ee1372ba637b097a93e6d60";
 export const BASE_URL = "https://api.themoviedb.org/3/movie/";
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
 export const language = "language=pt-BR";
--- main.js
+++ main.js
@@ -1,12 +1,40 @@
 import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
 
-const url =
-  "https://api.themoviedb.org/3/movie/550?api_key=ee6c522f6ee1372ba637b097a93e6d60";
+const $btn = document.querySelector("button");
+const $movieContainer = document.querySelector("[data-movie]");
+const $telaError = document.querySelector(".tela-error");
+
+const $img = document.querySelector("[data-movie] img");
+const $h2 = document.querySelector("[data-movie] h2");
+const $p = document.querySelector("[data-movie] p");
+
+let id = 2;
 
 const fetchMovie = async () => {
-  const movieResponse = await fetch(url);
-  const movieJson = await movieResponse.json();
-  console.log(movieJson);
+  try {
+    const url = `${BASE_URL}${id}?api_key=${API_KEY}`;
+    const movieResponse = await fetch(url);
+    const movieJson = await movieResponse.json();
+    const { title, overview, poster_path, status_message } = movieJson;
+    if (!status_message) {
+      if ($telaError) {
+        $telaError.classList.remove("active");
+      }
+      $movieContainer.classList.add("active");
+      $img.src = IMG_URL + poster_path;
+      $img.alt = title;
+      $h2.innerText = title;
+      $p.innerText = overview;
+    } else if (status_message) {
+      $movieContainer.classList.remove("active");
+      $telaError.classList.add("active");
+    }
+  } catch (erro) {
+    console.log(erro);
+  }
 };
 
-fetchMovie();
+$btn.addEventListener("click", () => {
+  fetchMovie();
+  id++;
+});
